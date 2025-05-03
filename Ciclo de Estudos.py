385e8b2ce0b9c002389257988766e04412204f2f
Pedro Lucas <dondinhow@gmail.com>
2025-05-02 23:03:09 -0400

    Primeiro Commit

---
+++ Ciclo de Estudos.py
@@ -0,0 +1,14 @@
+print('CRIADOR DE CICLO DE ESTUDO')
+mt = ['português','matemática','história','geografia','biologia','química','física']
+df = []
+hd = int(input('Diga uma quantidade de horas ideal para estudar por dia:'))
+for i in mt:
+  x = int(input(f'Qual é sua dificuldade em {i}?(de 1 a 5):'))
+  df.append(x)
+print('\nSeu Ciclo de Estudo será:')
+for j in range(len(mt)):
+  ar = round((df[j]*(hd*7/sum(df))))
+  if ar < 2:
+    print(f'- {mt[j]}: 2 horas')
+  else:
+    print(f'- {mt[j]}: {ar} horas')
---
+++ README.md
@@ -0,0 +1,3 @@
+*Ciclo de Estudo*
+Esse programa foi feito para a praticar o Python, além de facilitar a criação de um Ciclo de Estudo.
+OBS: O horário de estudo mínimo no programa é de duas horas, então em caso de uma matéria der menos de uma hora, será transformado em duas horas.
